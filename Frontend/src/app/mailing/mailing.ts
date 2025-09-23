fdb5e59ca3c52bea482c5e1fcc21a023eba44a37
76Moritz <motizbin@gmail.com>
2025-09-23 20:50:34 +0200

    set api key as secret

--- Frontend/src/app/mailing/mailing.ts
+++ Frontend/src/app/mailing/mailing.ts
@@ -5,13 +5,13 @@ import Mailgun from "mailgun.js"; // mailgun.js v11.1.0
   const mailgun = new Mailgun(FormData);
   const mg = mailgun.client({
     username: "api",
-    key:  "945cc234bb8a82e4fcac9751fe2daefb-e1076420-88d00dba",
+    key:  process.env["MAILGUN_API_KEY"] as string,
     // When you have an EU-domain, you must specify the endpoint:
     // url: "https://api.eu.mailgun.net"
   });
   try {
     const data = await mg.messages.create("sandbox289b1238590943aca464ab92b5f8f548.mailgun.org", {
-      from: "Mailgun Sandbox <postmaster@sandbox289b1238590943aca464ab92b5f8f548.mailgun.org>",
+      from: "Narrogruppe-Oberkirch<root@sandbox289b1238590943aca464ab92b5f8f548.mailgun.org>",
       to: ["Moritz Binder <moritz.binder03@gmail.com>"],
       subject: subject,
       text: text,
